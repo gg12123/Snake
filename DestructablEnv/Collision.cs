526e0917d37db41c0c21e9315ced7afeb5964b8b
George Godfrey <georgegodfrey24@gmail.com>
2018-07-15 19:51:57 +0100

    Added rigidbody and collision classes.

---
+++ DestructablEnv/Collision.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+   private const float e = 0.8f;
+
+   private Vector3 m_CollisionNormalWorld;
+   private Vector3 m_CollisionNormalBody1Local;
+   private Vector3 m_CollisionNormalBody2Local;
+
+   private Vector3 m_CollisionPointBody1Local;
+   private Vector3 m_CollisionPointBody2Local;
+
+   private float m_J;
+
+   private MyRigidbody m_Body1;
+   private MyRigidbody m_Body2;
+
+   public Vector3 GetImpulseWorld(MyRigidbody body)
+   {
+      return body == m_Body1 ? m_J * m_CollisionNormalWorld : -m_J * m_CollisionNormalWorld;
+   }
+
+   public Vector3 GetImpulseLocal(MyRigidbody body)
+   {
+      return body == m_Body1 ? m_J * m_CollisionNormalBody1Local : -m_J * m_CollisionNormalBody2Local;
+   }
+
+   public Vector3 GetCollisionPointLocal(MyRigidbody body)
+   {
+      return body == m_Body1 ? m_CollisionPointBody1Local : m_CollisionPointBody2Local;
+   }
+
+   public float CalculateS(Vector3 n, Vector3 r, MyRigidbody body)
+   {
+      var x = (Vector3)(body.InertiaInverse * Vector3.Cross(r, n));
+      return Vector3.Dot(n, Vector3.Cross(x, r));
+   }
+
+   public void Calculate(MyRigidbody body1, MyRigidbody body2, Vector3 collNormalWorld, Vector3 collPointWorld)
+   {
+      var v1 = body1.VelocityWorldAtPoint(collPointWorld);
+      var v2 = body2.VelocityWorldAtPoint(collPointWorld);
+
+      if (Vector3.Dot(v1, v2) < 0.0f)
+      {
+         var r1 = body1.transform.InverseTransformPoint(collPointWorld);
+         var r2 = body2.transform.InverseTransformPoint(collPointWorld);
+
+         m_CollisionNormalWorld = collNormalWorld;
+         m_CollisionNormalBody1Local = body1.transform.InverseTransformDirection(collNormalWorld);
+         m_CollisionNormalBody2Local = body2.transform.InverseTransformDirection(collNormalWorld);
+
+         var s1 = CalculateS(m_CollisionNormalBody1Local, r1, body1);
+         var s2 = CalculateS(m_CollisionNormalBody2Local, r2, body2);
+
+         var vr = v1 - v2;
+
+         var m1 = body1.Mass;
+         var m2 = body2.Mass;
+
+         m_J = (-Vector3.Dot(vr, m_CollisionNormalWorld) * (e + 1.0f)) / (1.0f / m1 + 1.0f / m2 + s1 + s2);
+
+         m_Body1 = body1;
+         m_Body2 = body2;
+         m_CollisionPointBody1Local = r1;
+         m_CollisionPointBody2Local = r2;
+
+         m_Body1.SetCollision(this);
+         m_Body2.SetCollision(this);
+      }
+   }
+}
---
+++ DestructablEnv/MyRigidbody.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRigidbody : MonoBehaviour
+{
+   [SerializeField]
+   private float m_Drag = 0.5f;
+   [SerializeField]
+   private float m_AngularDrag = 0.5f;
+
+   private const float g = 9.8f;
+
+   public Vector3 VelocityWorld { get; private set; }
+   public Vector3 VelocityLocal { get { return transform.InverseTransformDirection(VelocityWorld); } }
+   public Vector3 AngularVelocityWorld { get { return transform.TransformDirection(AngularVelocityLocal); } }
+   public Vector3 AngularVelocityLocal { get; private set; }
+   public float Mass { get; private set; }
+   public Matrix4x4 Inertia { get; private set; }
+   public Matrix4x4 InertiaInverse { get; private set; }
+
+   private Collision m_Collision;
+
+   public void Init()
+   {
+      // calculate inertia using the points in shape
+      var shape = GetComponent<Shape>();
+   }
+
+   public void SetCollision(Collision col)
+   {
+      m_Collision = col;
+   }
+
+   private void CalculateForces(out Vector3 forcesWorld, out Vector3 momentsLocal)
+   {
+      forcesWorld = Vector3.zero;
+      forcesWorld -= Mass * g * Vector3.up;
+      forcesWorld -= m_Drag * VelocityWorld;
+
+      momentsLocal = Vector3.zero;
+      momentsLocal -= m_AngularDrag * AngularVelocityLocal;
+   }
+
+   private Quaternion AngularVelAsQuat()
+   {
+      return new Quaternion(AngularVelocityLocal.x, AngularVelocityLocal.y, AngularVelocityLocal.z, 0.0f);
+   }
+
+   private Quaternion QMul(Quaternion q, float m)
+   {
+      return new Quaternion(q.x * m, q.y * m, q.z * m, q.w * m);
+   }
+
+   private Quaternion QAdd(Quaternion q1, Quaternion q2)
+   {
+      return new Quaternion(q1.x + q2.x, q1.y + q2.y, q1.z + q2.z, q1.w + q2.w);
+   }
+
+   private void Integrate(Vector3 forcesWorld, Vector3 momentsLocal)
+   {
+      VelocityWorld += (forcesWorld / Mass) * Time.deltaTime;
+      transform.position += VelocityWorld * Time.deltaTime;
+
+      AngularVelocityLocal += (Vector3)(InertiaInverse * momentsLocal * Time.deltaTime);
+      var q = transform.rotation;
+      transform.rotation = QAdd(q, QMul(q * AngularVelAsQuat(), 0.5f * Time.deltaTime));
+   }
+
+   private void ApplyImpulse()
+   {
+      VelocityWorld += m_Collision.GetImpulseWorld(this) / Mass;
+
+      var r = m_Collision.GetCollisionPointLocal(this);
+      var J = m_Collision.GetImpulseLocal(this);
+      AngularVelocityLocal += (Vector3)(InertiaInverse * Vector3.Cross(r, J));
+   }
+
+   public void UpdateSimulation()
+   {
+      if (m_Collision == null)
+      {
+         Vector3 f, m;
+         CalculateForces(out f, out m);
+         Integrate(f, m);
+      }
+      else
+      {
+         ApplyImpulse();
+         m_Collision = null;
+      }
+   }
+
+   public Vector3 VelocityWorldAtPoint(Vector3 pointWorld)
+   {
+      return VelocityWorld + Vector3.Cross(AngularVelocityWorld, pointWorld - transform.position);
+   }
+}
